d427c295515ba2515aea48fee66fb3791ecbf037
sergeykon <beergood6@gmail.com>
2020-02-02 23:11:47 +0300

    added file reader and logger. Refactor all app.

--- calc-price/src/main/java/epam/price/Main.java
+++ calc-price/src/main/java/epam/price/Main.java
@@ -1,71 +1,62 @@
 package epam.price;
 
+import org.apache.log4j.Logger;
+
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
-import static epam.price.Odds.*;
+import static epam.price.Odds.getPricePerKilogram;
+import static epam.price.Odds.getPricePerKilometer;
 
 public class Main {
 
-    //TODO Добавить возможность указать коэффициенты, и изменить их при необходимости
-    public static void main(String[] args) throws IOException {
+    final static Logger logger = Logger.getLogger(Main.class.getName());
+    private static Scanner scanner = new Scanner(System.in);
 
-//        Map<Double, Double> pricePerKilogram = new HashMap<Double, Double>();
-//        pricePerKilogram.put(0d, 20d);
-//        pricePerKilogram.put(500d, 17d);
-//        pricePerKilogram.put(1000d, 15d);
-//
-//        Map<Double, Double> pricePerKilometer = new HashMap<Double, Double>();
-//        pricePerKilometer.put(0d, 10d);
-//        pricePerKilometer.put(500d, 7.3d);
-//        pricePerKilometer.put(1000d, 6.6d);
-//
-//        setPricePerKilogram(pricePerKilogram);
-//        setPricePerKilometer(pricePerKilometer);
-//
-//        Path currentRelativePath = Paths.get("");
-//        String s = currentRelativePath.toAbsolutePath().toString();
-//        System.out.println("Current relative path is: " + s);
+    public static void main(String[] args) {
 
         Double[] enteredValues = new Double[2];
-
-        Scanner scanner = new Scanner(System.in);
-
-        setPricesPerKilogram();
-        setPricesPerKilometer();
         int i = 0;
 
         do {
-            switch (i){
-
-                case 0 : {
-                    System.out.println("Please enter distance or Q for exit: ");
+            switch (i) {
+                case 0: {
+                    System.out.println("Please, enter distance or Q for exit: ");
                     break;
                 }
-                case 1 : {
+                case 1: {
                     System.out.println("Please, enter weight or Q for exit: ");
                     break;
                 }
             }
-
-
             if(scanner.hasNextDouble()) {
                 enteredValues[i] = scanner.nextDouble();
-                i++;
-            } else if(scanner.next().equalsIgnoreCase("Q")) break;
+                logger.info("User enter value " + enteredValues[i]);
+                if(enteredValues[i] < 0){
+                    logger.info("Value " + enteredValues[i] + " not accepted");
+                    System.out.println("Number must be positive");
+                } else i++;
+            } else if(scanner.next().equalsIgnoreCase("Q")) {
+                logger.info("User quit from program");
+                break;
+            }
+            if (i == 2) {
+                try {
+                    System.out.println("Price: $" + (Double) (enteredValues[0] * getPricePerKilometer(enteredValues[0]) +
+                            enteredValues[1] * getPricePerKilogram(enteredValues[1])));
 
-            if (i == 2){
-                System.out.println("Price: $" + (Double) (enteredValues[0]*getPricePerKilometer(enteredValues[0]) +
-                        enteredValues[1]*getPricePerKilogram(enteredValues[1])));
-                i = 0;
+                    logger.info("Distance = " + enteredValues[0] + " Price per km = " + getPricePerKilometer(enteredValues[0]) +
+                            " Weight = " + enteredValues[1] + " Price per kg = " + getPricePerKilogram(enteredValues[1]) +
+                            " Cost of delivery = " + ((Double) (enteredValues[0] * getPricePerKilometer(enteredValues[0]) +
+                            enteredValues[1] * getPricePerKilogram(enteredValues[1]))));
+                    i = 0;
+                } catch (IOException e) {
+                    logger.error("Can't take value from file:\n" + e.getMessage());
+                    i = 2;
+                }
             }
         } while (true);
-
         System.out.println("Finish!");
-
+        logger.info("Program completed");
     }
 }
--- calc-price/src/main/java/epam/price/Odds.java
+++ calc-price/src/main/java/epam/price/Odds.java
@@ -2,75 +2,55 @@ package epam.price;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
+import org.apache.log4j.Logger;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Odds {
+
     private static Map<Double, Double> pricesPerKilogram = new HashMap<>();
     private static Map<Double, Double> pricesPerKilometer = new HashMap<>();
+    private final static Logger logger = Logger.getLogger(Odds.class);
 
-    private static TypeReference<Map<Double, Double>> typeReference = new TypeReference<Map<Double, Double>>(){};
+    private static String filePathPricePerKilogram ="src/main/resources/pricePerKilogram";
+    private static String filePathPricePerKilometer ="src/main/resources/pricePerKilometer";
 
-//TODO Подключение к файлам и их создание при отсутствии. Нейминг функций
-
-
-    private static Map<Double,Double> getPricesFromFile(String filePath) throws IOException {
-        return JSON.parseObject(filePath, new TypeReference<Map<Double, Double>>(){});
+    private static Map<Double,Double> takePricesFromFile(String filePath) throws IOException {
+        InputStream inputStream = new FileInputStream(filePath);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        String jsonLine = bufferedReader.readLine();
+        logger.info("JSON successfully taken from " + filePath + " file");
+        return JSON.parseObject(jsonLine, new TypeReference<Map<Double, Double>>(){});
     }
 
-
-    public static void setPricesPerKilogram() throws IOException {
-        pricesPerKilogram = getPricesFromFile(
-                "{0.0:20.0,500.0:17.0,1000.0:15.0}");
+    public static Double getPricePerKilogram(Double kilograms) throws IOException {
+        if(pricesPerKilogram.isEmpty())
+            setPricesPerKilogram();
+        return getPrice(kilograms, pricesPerKilogram);
     }
 
-    public static void setPricesPerKilometer() throws IOException {
-        pricesPerKilometer = getPricesFromFile(
-                "{0.0:10.0,500.0:7.3,1000.0:6.6}");
+    public static Double getPricePerKilometer(Double kilometers) throws IOException {
+        if(pricesPerKilogram.isEmpty())
+            setPricesPerKilometer();
+        return getPrice(kilometers, pricesPerKilometer);
     }
 
-
-    private static void writePriceToFile(Map<Double, Double> writingMap, String fileName) throws IOException {
-        FileWriter jsonFileWithPrice = new FileWriter(fileName, false);
-        jsonFileWithPrice.write(JSON.toJSONString(writingMap));
-        jsonFileWithPrice.flush();
-    }
-
-    public static void setPricePerKilogram(Map<Double,Double> price) throws IOException {
-        pricesPerKilogram = price;
-        writePriceToFile(pricesPerKilogram,
-                "/home/sergeykon/Desktop/Work/epam/practice/skononovich-price-calc/calc-price/src/main/resources/pricePerKilogram");
-    }
-
-    public static void setPricePerKilometer(Map<Double, Double> price) throws IOException {
-        pricesPerKilometer = price;
-        writePriceToFile(pricesPerKilometer,
-                "/home/sergeykon/Desktop/Work/epam/practice/skononovich-price-calc/calc-price/src/main/resources/pricePerKilometer");
-    }
-
-    public static Double getPricePerKilogram(Double kilograms){
+    private static Double getPrice(Double value, Map<Double, Double> mapForSearching){
         Double price = 0d;
-
-        for(Map.Entry<Double, Double>  costPerKilogram : pricesPerKilogram.entrySet()) {
-            if(costPerKilogram.getKey() < kilograms)
-                price = costPerKilogram.getValue();
+        for(Map.Entry<Double, Double> itemFromMap : mapForSearching.entrySet()) {
+            if(itemFromMap.getKey() <= value)
+                price = itemFromMap.getValue();
         }
         return price;
     }
 
-    public static Double getPricePerKilometer(Double kilometers){
-        Double price = 0d;
+    private static void setPricesPerKilogram() throws IOException {
+        pricesPerKilogram = takePricesFromFile(filePathPricePerKilogram);
+    }
 
-        for(Map.Entry<Double, Double> costPerKilometer : pricesPerKilometer.entrySet()) {
-            if(costPerKilometer.getKey() < kilometers)
-                price = costPerKilometer.getValue();
-        }
-        return price;
+    private static void setPricesPerKilometer() throws IOException {
+        pricesPerKilometer = takePricesFromFile(filePathPricePerKilometer);
     }
 }
